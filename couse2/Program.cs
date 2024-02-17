c6c6ff15f916a30651c457a89f280ae7064ccd93
mailon cesar luiz <mailon cesar luiz@CESAR>
2024-02-17 11:37:34 -0300

    Add project files.

---
+++ couse2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
+
+namespace PrimeiroProjeto {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != 2002) {
+                Console.WriteLine("SENHA INVALIDA");
+                 n = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("ACESSO PERMITIDO");
+
+
+
+
+
+        }
+    } 
+}
+
+
+
+
+
+
+
+
